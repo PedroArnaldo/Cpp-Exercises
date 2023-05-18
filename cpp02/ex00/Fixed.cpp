27647dff236a571ba678d424264e43894e1629b6
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-05-18 18:30:27 -0300

    feat: cpp02 ex00 ready

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ): _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+Fixed::~Fixed( void )
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(Fixed const &obj)
+{
+    std::cout << "Copy assigment operator called" << std::endl;
+    this->_value = obj.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const 
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->_value);
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_value = raw;
+}
