e83ce979f1b3a18808712a40427df9e26374f432
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-05-19 16:58:53 -0300

    feat: created ex02, done comparison operators

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,90 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void): _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+Fixed::Fixed(const int num) : _value(num << this->bitsFixedPoint)
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float num) : _value(int(round((num * (1 << this->bitsFixedPoint)))))
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(Fixed const &obj)
+{
+    std::cout << "Copy assigment operator called" << std::endl;
+    this->_value = obj.getRawBits();
+    return *this;
+}
+
+bool  Fixed::operator>(Fixed const &obj)
+{
+    return this->_value > obj._value;
+}
+
+bool  Fixed::operator<(Fixed const &obj)
+{
+    return this->_value < obj._value;
+}
+
+bool  Fixed::operator>=(Fixed const &obj)
+{
+    return this->_value >= obj._value;
+}
+
+bool  Fixed::operator<=(Fixed const &obj)
+{
+    return this->_value <= obj._value;
+}
+
+bool  Fixed::operator==(Fixed const &obj)
+{
+    return this->_value == obj._value;
+}
+
+bool  Fixed::operator!=(Fixed const &obj)
+{
+    return this->_value != obj._value;
+}
+
+int Fixed::getRawBits(void) const 
+{
+    return (this->_value);
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
+}
+
+int     Fixed::toInt(void) const
+{
+    return (this->_value >>  this->bitsFixedPoint);
+}
+
+float   Fixed::toFloat(void) const
+{
+    return ((float(this->_value) / (1 << this->bitsFixedPoint)));
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &obj)
+{
+    os << obj.toFloat();
+    return (os);
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,38 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed{
+
+    public:
+
+        Fixed(void);
+        Fixed(Fixed const &src);
+        Fixed(const int num);
+        Fixed(const float num);
+        ~Fixed(void);
+
+        Fixed &operator=(Fixed const &obj);
+        bool operator>(Fixed const &obj);
+        bool operator<(Fixed const &obj);
+        bool operator>=(Fixed const &obj);
+        bool operator<=(Fixed const &obj);
+        bool operator==(Fixed const &obj);
+        bool operator!=(Fixed const &obj);
+
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+        int     toInt(void) const;
+        float   toFloat(void) const;
+    
+    private:
+        int _value;
+        static const int bitsFixedPoint = 8;
+
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &obj);
+
+#endif
