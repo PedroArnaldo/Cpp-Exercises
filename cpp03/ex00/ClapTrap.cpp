540bcdc6fc605afa6f887223090cb315a43b52f3
Pedro Arnaldo <parnaldo@student.42.rio>
2023-05-20 18:36:11 -0300

    wp: cpp03 ex00 clapTrap - constructors getters setters

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -1,6 +1,6 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(std::string name): _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+ClapTrap::ClapTrap(std::string name): _name(name), _hitPoint(10), _energyPoint(10), _attackDamage(0)
 {
 }
 
@@ -29,6 +29,26 @@ void ClapTrap::takeDamage(unsigned int amount)
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
+{
+
+}
+
+void ClapTrap::setName(std::string name)
+{
+    this->_name = name;
+}
+
+void ClapTrap::setHitPoint(int hitPoint)
+{
+    this->_hitPoint = hitPoint;
+}
+
+void ClapTrap::setEnergyPoint()
+{
+    
+}
+
+void ClapTrap::setAttackDamage()
 {
 
 }
