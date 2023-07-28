90ab47238db2dc96f049662b671e85076f67dc3b
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-07-28 18:01:52 -0300

    feat: creat form and cpp01

--- cpp05/ex01/src/Form.cpp
+++ cpp05/ex01/src/Form.cpp
@@ -1,23 +1,44 @@
 #include "Form.hpp"
 
-Form::Form()
+Form::Form(std::string name, int gradeSign, int gradeExecute) : 
+	_name(name),
+	_gradeSign(gradeSign),
+	_gradeExecute(gradeExecute)
 {
+	try
+	{
+		if (gradeSign < 1 || gradeExecute < 1)
+			throw Form::GradeTooLowExeception();
+		if (gradeSign > 150 || gradeExecute > 150)
+			throw Form::GradeTooHighExecption();
+	}
+	catch (const Form::GradeTooLowExeception& e)
+	{
+		std::cerr << e.what() << '\n'; 
+	}
+	catch (const Form::GradeTooHigh'Exeception& e)
+	{
+		std::cerr << e.what() << '\n'; 
+	}
+
 }
 
 Form::Form(const Form& src)
 {
+	std::cout << "Form copy constructor called.'" << std::endl;
 }
 
 Form::~Form()
 {
+	std::cout << "Form " << this->_name << " is destroyed." << std::endl;
 }
 
 Form&	Form::operator=(Form const& rhs)
 {
-	//if ( this != &rhs )
-	//{
-		//this->_value = rhs.getValue();
-	//}
+	if (this != &rhs)
+	{
+		this->_sign = rhs.getSign();
+	}
 	return *this;
 }
 
@@ -39,6 +60,17 @@ int Form::getGradeSign() const
 int Form::getGradeExecute() const
 {
 	return this->_gradeExecute;
+}
+
+void Form::beSigned(Bureaucrat &bureaucrat)
+{
+	try
+	{
+	
+	}
+	catch
+	{
+	}
 }
 
 std::ostream& operator<<( std::ostream & o, Form const & i )
