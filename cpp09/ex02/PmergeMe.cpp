aae06fa235611f73b6d6fa7c3c8d47900213e95d
Pedro Arnaldo madeira lopes <parnaldo@c2t5s1.42.rio>
2023-09-09 17:54:07 -0300

    feat: sort vector

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -59,9 +59,91 @@ void PmergeMe::result(char **arg)
         _deque.push_back(num);
         i++;
     }
+    
+    std::cout << " Before: ";
+    for (size_t i = 0; i < _vector.size(); i++)
+    {
+        std::cout << _vector[i] << " ";
+    }
+    std::cout << std::endl;
+    mergeInsertSortVector(_vector, 0, _vector.size() - 1);   
+   
+    for (size_t i = 0; i < _vector.size(); i++)
+    {
+        std::cout << _vector[i] << " ";
+    }
 }
 
 const char* PmergeMe::invalidArgException::what() const throw()
 {
     return "Error: input invalid.";
+}
+
+//Sort vector
+void PmergeMe::insertionSortVector(std::vector<unsigned int> &vector)
+{
+    for (unsigned int i = 1; i < vector.size(); i++)
+    {
+        int currentIndex = i;
+        while (currentIndex > 0 && vector[currentIndex - 1] > vector[currentIndex])
+        {
+            int temp = vector[currentIndex];
+            vector[currentIndex] = vector[currentIndex - 1];
+            vector[currentIndex - 1] = temp;
+            currentIndex--;
+        }
+    }
+}
+
+void PmergeMe::mergeSortVector(std::vector<unsigned int> &vector, unsigned int start, unsigned  int mid,  unsigned int end)
+{
+    std::vector<unsigned int> leftVect(mid - start + 1);
+    std::vector<unsigned int> rightVec(end - mid);
+
+    for (unsigned int i = 0; i < leftVect.size(); ++i)
+        leftVect[i] = vector[start + i];
+
+    for (unsigned int i = 0; i < rightVec.size(); ++i)
+        rightVec[i] = vector[mid + 1 + i];
+    
+    unsigned int leftIndex = 0, rightIndex = 0;
+
+    unsigned int currentIndex = start;
+
+    while (leftIndex < leftVect.size() && rightIndex < rightVec.size())
+    {
+        if (leftVect[leftIndex] <= rightVec[rightIndex]){
+            vector[currentIndex] = leftVect[leftIndex];
+            leftIndex++;
+        }
+        else {
+            vector[currentIndex] = rightVec[rightIndex];
+            rightIndex++;
+        }
+        currentIndex++;
+    }
+
+    while (leftIndex < leftVect.size())
+        vector[currentIndex++] = leftVect[leftIndex++];
+
+    while (rightIndex  < rightVec.size())
+        vector[currentIndex++] = rightVec[rightIndex++];
+}
+
+void PmergeMe::mergeInsertSortVector(std::vector<unsigned int> &vector, unsigned int start, unsigned int end)
+{
+    unsigned int middle = 0;
+   // unsigned int size = vector.size();
+
+    // if (size < 16){
+    //     insertionSortVector(vector);
+    // }
+    //else{
+       middle = (start + end) / 2;
+
+       mergeInsertSortVector(vector, start, middle);
+       mergeInsertSortVector(vector, middle + 1, end);
+
+      mergeSortVector(vector, start, middle, end);
+    //}
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -5,6 +5,7 @@
 #include <exception>
 #include <vector>
 #include <deque>
+#include <stdlib.h>
 
 class PmergeMe
 {
@@ -15,6 +16,11 @@ class PmergeMe
         bool checkInput(char **arg);
         std::vector<unsigned int>  _vector;
         std::deque<unsigned int> _deque;
+        void mergeInsertSortVector(std::vector<unsigned int> &vector, unsigned int start, unsigned int end);
+        void mergeInsertSortDeque(std::deque<unsigned int> &deque);
+        void insertionSortVector(std::vector<unsigned int> &vector);
+        void mergeSortVector(std::vector<unsigned int> &vector, unsigned int start, unsigned int middle, unsigned int end);
+
 
     public:
         PmergeMe(char **arg);
