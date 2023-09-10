c38f5c96b895c6352a1ea2cd6009d46786babb1c
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-09-10 17:11:39 -0300

    feat: done cpp09

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -8,7 +8,7 @@ PmergeMe::PmergeMe(char **arg)
 {
     if (!checkInput(arg))
         throw invalidArgException();
-    result(arg);
+    execute(arg);
 }
 
 PmergeMe::PmergeMe(const PmergeMe &cpy)
@@ -46,7 +46,7 @@ bool PmergeMe::checkInput(char **arg)
     return (true);
 }
 
-void PmergeMe::result(char **arg)
+void PmergeMe::fillContainers(char **arg)
 {
     unsigned int num;
     int i = 1;
@@ -59,24 +59,39 @@ void PmergeMe::result(char **arg)
         _deque.push_back(num);
         i++;
     }
-    
-    std::cout << " Before: ";
-    for (size_t i = 0; i < _vector.size(); i++)
-    {
-        std::cout << _vector[i] << " ";
-    }
-    std::cout << std::endl;
-    mergeInsertSortVector(_vector, 0, _vector.size() - 1);   
-   
-    for (size_t i = 0; i < _vector.size(); i++)
-    {
-        std::cout << _vector[i] << " ";
-    }
 }
 
-const char* PmergeMe::invalidArgException::what() const throw()
+void PmergeMe::execute(char **arg)
 {
-    return "Error: input invalid.";
+    clock_t startVector;
+    clock_t endVector;
+    clock_t startDeque;
+    clock_t endDeque;
+    unsigned int size;
+
+    fillContainers(arg);
+
+    size = _vector.size();
+
+    printVector("Before");
+
+    
+    startVector = clock();
+    mergeInsertSortVector(_vector, 0, _vector.size() - 1);
+    endVector = clock();
+
+    startDeque = clock();
+    mergeInsertSortDeque(_deque, 0, _deque.size() - 1);
+    endDeque = clock();
+
+    printVector("After");
+
+    double timeVector = (double)(endVector - startVector) / CLOCKS_PER_SEC;
+    double timeDeque = (double)(endDeque - startDeque) / CLOCKS_PER_SEC;
+
+    std::cout << "Time to process a range of " << size << " elemenst withs std::vector : " << timeVector * 1000000.0 << " us" << std::endl;
+    std::cout << "Time to process a range of " << size << " elemenst withs std::deque : " << timeDeque  * 1000000.0 << " us" << std::endl;
+
 }
 
 //Sort vector
@@ -132,18 +147,111 @@ void PmergeMe::mergeSortVector(std::vector<unsigned int> &vector, unsigned int s
 
 void PmergeMe::mergeInsertSortVector(std::vector<unsigned int> &vector, unsigned int start, unsigned int end)
 {
-    unsigned int middle = 0;
-   // unsigned int size = vector.size();
+   unsigned int middle = (start + end) / 2;
+   unsigned int size = start - end;
 
-    // if (size < 16){
-    //     insertionSortVector(vector);
-    // }
-    //else{
-       middle = (start + end) / 2;
-
+    if (size < 16){
+        insertionSortVector(vector);
+    }
+    else{
        mergeInsertSortVector(vector, start, middle);
        mergeInsertSortVector(vector, middle + 1, end);
+    }
+    mergeSortVector(vector, start, middle, end);
+}
 
-      mergeSortVector(vector, start, middle, end);
-    //}
+//Sort deque
+void PmergeMe::insertionSortDeque(std::deque<unsigned int> &deque)
+{
+    for (unsigned int i = 1; i < deque.size(); i++)
+    {
+        int currentIndex = i;
+        while (currentIndex > 0 && deque[currentIndex - 1] > deque[currentIndex])
+        {
+            int temp = deque[currentIndex];
+            deque[currentIndex] = deque[currentIndex - 1];
+            deque[currentIndex - 1] = temp;
+            currentIndex--;
+        }
+    }
+}
+
+void PmergeMe::mergeSortDeque(std::deque<unsigned int> &deque, unsigned int start, unsigned  int mid,  unsigned int end)
+{
+    std::deque<unsigned int> leftDeque(mid - start + 1);
+    std::deque<unsigned int> rightDeque(end - mid);
+
+    for (unsigned int i = 0; i < leftDeque.size(); ++i)
+        leftDeque[i] = deque[start + i];
+
+    for (unsigned int i = 0; i < rightDeque.size(); ++i)
+        rightDeque[i] = deque[mid + 1 + i];
+    
+    unsigned int leftIndex = 0, rightIndex = 0;
+
+    unsigned int currentIndex = start;
+
+    while (leftIndex < leftDeque.size() && rightIndex < rightDeque.size())
+    {
+        if (leftDeque[leftIndex] <= rightDeque[rightIndex]){
+            deque[currentIndex] = leftDeque[leftIndex];
+            leftIndex++;
+        }
+        else {
+            deque[currentIndex] = rightDeque[rightIndex];
+            rightIndex++;
+        }
+        currentIndex++;
+    }
+
+    while (leftIndex < leftDeque.size())
+        deque[currentIndex++] = leftDeque[leftIndex++];
+
+    while (rightIndex  < rightDeque.size())
+        deque[currentIndex++] = rightDeque[rightIndex++];
+}
+
+void PmergeMe::mergeInsertSortDeque(std::deque<unsigned int> &deque, unsigned int start, unsigned int end)
+{
+   unsigned int middle = (start + end) / 2;
+   unsigned int size = start - end;
+
+    if (size < 16){
+        insertionSortDeque(deque);
+    }
+    else{
+       mergeInsertSortDeque(deque, start, middle);
+       mergeInsertSortDeque(deque, middle + 1, end);
+    }
+    mergeSortDeque(deque, start, middle, end);
+}
+
+void PmergeMe::printVector(std::string type)
+{
+    std::cout << type << ":\t";
+
+    if (_vector.size() > 10)
+    {
+        for (unsigned int i = 0; 
+        i <= 5; i++)
+        {
+            std::cout << _vector[i] << "\t";
+        }
+        std::cout << "[..]";
+    }
+    else
+    {
+        for (std::vector<unsigned int> ::const_iterator it = _vector.begin(); 
+            it != _vector.end(); ++it)
+        {
+            std::cout << *it << " ";
+        }
+    }
+    std::cout << std::endl;
+    
+}
+
+const char* PmergeMe::invalidArgException::what() const throw()
+{
+    return "Error: input invalid.";
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -7,25 +7,36 @@
 #include <deque>
 #include <stdlib.h>
 
+
 class PmergeMe
 {
     private:
         PmergeMe();
         PmergeMe(const PmergeMe &cpy);
         PmergeMe& operator=(const PmergeMe &src);
+
         bool checkInput(char **arg);
+        
         std::vector<unsigned int>  _vector;
         std::deque<unsigned int> _deque;
+
         void mergeInsertSortVector(std::vector<unsigned int> &vector, unsigned int start, unsigned int end);
-        void mergeInsertSortDeque(std::deque<unsigned int> &deque);
         void insertionSortVector(std::vector<unsigned int> &vector);
         void mergeSortVector(std::vector<unsigned int> &vector, unsigned int start, unsigned int middle, unsigned int end);
+
+
+        void mergeInsertSortDeque(std::deque<unsigned int> &deque, unsigned int start, unsigned int end);
+        void insertionSortDeque(std::deque<unsigned int> &deque);
+        void mergeSortDeque(std::deque<unsigned int> &deque, unsigned int start, unsigned int middle, unsigned int end);
+
+        void fillContainers(char **arg);
+        void printVector(std::string type);
 
 
     public:
         PmergeMe(char **arg);
         ~PmergeMe();
-        void result(char **arg);
+        void execute(char **arg);
     
         class invalidArgException : public std::exception
         {
